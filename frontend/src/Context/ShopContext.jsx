9dcb71976e0e7175560bfb408330b97ace98a2bc
atef5922 <sifat15-5922@diu.edu.bd>
2025-11-25 03:43:03 +0600

    Navbar

---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,48 @@
+import React, { createContext, useState } from 'react'
+
+export const ShopContext = createContext(null)
+
+const getDefaultCart = () => {
+  return {}
+}
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart())
+
+  const getTotalCartItems = () => {
+    let totalItems = 0
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItems += cartItems[item]
+      }
+    }
+    return totalItems
+  }
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: (prev[itemId] || 0) + 1 }))
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: Math.max((prev[itemId] || 0) - 1, 0) }))
+  }
+
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: Math.max(newAmount, 0) }))
+  }
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    updateCartItemCount,
+    getTotalCartItems
+  }
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  )
+}
+
