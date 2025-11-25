55cedbac21bc117057b074b5c3d509292ea8c8a3
atef5922 <sifat15-5922@diu.edu.bd>
2025-11-26 01:44:01 +0600

    RelatedProduct Section Done

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Bredcrums from '../Components/Bredcrums/BredCrums';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import Description from '../Components/Description/Description';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -14,6 +15,7 @@ const Product = () => {
       <Bredcrums product={product}/>
       <ProductDisplay product={product}/>
       <Description/>
+      <RelatedProducts/>
     </div>
   )
 }
